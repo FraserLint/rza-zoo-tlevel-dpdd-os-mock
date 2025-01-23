ce6b5759dcc3b5c12146aee69560f02c68488dea
FraserLint <davisonaustin287@gmail.com>
2025-01-23 20:43:33 +0000

    Refactored nav and footer component instansiation to affect all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Content from "@/components/Content";
 import OurAnimals from "@/components/OurAnimals";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-2">      
-      <Navbar />
-      <main>
-        <Hero />
-        <Content>
-          <OurAnimals />
-        </Content>
-      </main>
-      <Footer />
-    </div>
+    <main>
+      <Hero />
+      <Content>
+        <OurAnimals />
+      </Content>
+    </main>
   );
 }
