367892fcd0ce63b01d8351c0c3f60d37b82f3181
FraserLint <davisonaustin287@gmail.com>
2025-01-23 19:01:31 +0000

    Updated Content component background colour

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,6 @@
 export default function Content({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full p-2 bg-[var(--dark-brown)] rounded-lg border-4 border-[var(--forest-green)]">
+    <div className="w-full p-2 bg-[var(--moss-green)] rounded-lg border-4 border-[var(--forest-green)]">
       {children}
     </div>
   );
